baa17d492a419bace669e0b547cf5b44a1204a91
zihao <yaozihao@yaozihao.com>
2026-02-16 14:23:18 +1100

    chore(rag): delete /admin/content page and admin-content actions

--- src/app/(protected)/admin/content/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getAdminDocuments, getAdminExamPapers } from '@/app/actions/admin-content';
-import { AdminContentClient } from './AdminContentClient';
-
-export default async function AdminContentPage() {
-  const [documents, examPapers] = await Promise.all([getAdminDocuments(), getAdminExamPapers()]);
-
-  return <AdminContentClient documents={documents} examPapers={examPapers} />;
-}
